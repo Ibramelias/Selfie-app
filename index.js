78731f9dea03a9d1f831a5963260d0110f77c068
Ibrameias <ibramdarwish@gmail.com>
2021-05-20 12:40:01 -0400

    use nedb to creating database to store the clinet data

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const { request, response } = require('express');
 const express = require('express')
+const Datastore = require('nedb');
 const app = express();
 app.listen(3000, () => console.log("Listening at port 3000"));
 app.use(express.static('public'))
 app.use(express.json({limit: "1mb"}))
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 
 app.post('/api', (request,response) =>{
     console.log(request.body)
     const data = request.body;
+    database.insert(data);
+    console.log(database);
     response.json({
         status: 'success',
         latitude: data.lat,
